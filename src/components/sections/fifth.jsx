5492199e23fcd95ed1650a68a3b5fbd8c3af2686
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2023-02-16 20:25:59 +0100

    ah

--- src/components/sections/fifth.jsx
+++ src/components/sections/fifth.jsx
@@ -1,44 +1,45 @@
-import styles from "./fifth.module.css"
+// import styles from "./fifth.module.css"
 
 
 const Fifthsection = () => {
     return(
-        <div>
-            <div className={styles.wrapdiv}>
-                <ul>
-                    <li>
-                        <h1></h1>
-                        <div>Balance sheet</div>
-                    </li>
-                    <li>
-                        <h1></h1>
-                        <div>Cash flow</div>
-                    </li>
-                    <li>
-                        <h1></h1>
-                        <div>Payroll management</div>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <h1></h1>
-                        <div>Automated data entry</div>
-                    </li>
-                    <li>
-                        <h1></h1>
-                        <div>OCR</div>
-                    </li>
-                    <li>
-                        <h1></h1>
-                        <div>Payroll management</div>
-                    </li>
-                </ul>
-                <li>
-                    <h1></h1>
-                    <div>Profit & loss statement</div>
-                </li>
-            </div>
-        </div>
+        <></>
+        // <div>
+        //     <div className={styles.wrapdiv}>
+        //         <ul>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>Balance sheet</div>
+        //             </li>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>Cash flow</div>
+        //             </li>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>Payroll management</div>
+        //             </li>
+        //         </ul>
+        //         <ul>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>Automated data entry</div>
+        //             </li>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>OCR</div>
+        //             </li>
+        //             <li>
+        //                 <h1></h1>
+        //                 <div>Payroll management</div>
+        //             </li>
+        //         </ul>
+        //         <li>
+        //             <h1></h1>
+        //             <div>Profit & loss statement</div>
+        //         </li>
+        //     </div>
+        // </div>
     )
 }
 
